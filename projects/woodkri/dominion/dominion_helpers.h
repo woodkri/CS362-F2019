80381d7f00653303337b77f2ad2a8848b511f593
Kristin <kristin.wood@eduworks.com>
2019-10-05 19:44:15 -0400

    Refactor code into functions for cardEffect

--- projects/woodkri/dominion/dominion_helpers.h
+++ projects/woodkri/dominion/dominion_helpers.h
@@ -11,5 +11,10 @@ int gainCard(int supplyPos, struct gameState *state, int toFlag, int player);
 int getCost(int cardNumber);
 int cardEffect(int card, int choice1, int choice2, int choice3,
                struct gameState *state, int handPos, int *bonus);
+int cardEffectBaron(int choice1, struct gameState *state, int currentPlayer);
+int cardEffectMine(int choice1, int choice2, struct gameState *state, int handPos, int currentPlayer);
+int cardEffectMinion(int choice1, int choice2, struct gameState *state, int handPos, int currentPlayer);
+int cardEffectAmbassador(int choice1, int choice2, struct gameState *state, int handPos, int currentPlayer);
+int cardEffectTribute(struct gameState *state, int currentPlayer, int nextPlayer, int* tributeRevealedCards);
 
 #endif
